49e3d65a812ec8ffef121f4c868d80da39f7883e
Andy Arce <andyiarce@gmail.com>
2023-04-06 14:45:05 -0500

    Added a total to the cart

--- scripts.js
+++ scripts.js
@@ -26,6 +26,51 @@ function addToCartClick(event) {
     var itemName = itemClicked.querySelector('.card-title').innerText
     var itemCost = itemClicked.querySelector('.card-price').innerText
     var itemImage = itemClicked.querySelector('.card-img-top').src
-    console.log(itemName, itemCost, itemImage)
+    createIteminCart(itemName, itemCost, itemImage)
 }
 
+function createIteminCart(title, price, imgSrc){
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('itemsInCart')[0]
+
+    var cartItemsNames = cartItems.getElementsByClassName('cart-iten-name')
+    for(var i = 0; i < cartItemsNames.length; i++){
+        if(cartItemsNames[i].innerText == title) {
+            alert('This item is already in the cart.')
+            return
+        }
+    }
+
+    var cartRowContents = `                                        
+    <div class="row py-1">
+        <div class="col">
+            <img class="rounded w-100" src="${imgSrc}" alt="">
+        </div>
+        <div class="col cart-item-name">${title}</div>
+        <div class="col cart-item-price"> ${price} </div>
+        <div class="col">
+            <input type="number" class="cart-item-amount" value="1" min="1" max="50" size="1">
+        </div>
+    </div>
+    `
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    updateCartTotal()
+}
+
+function updateCartTotal(){
+    var cartItemContainer = document.getElementsByClassName('itemsInCart')[0]
+    cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    total = 0
+    for(var i = 0; i< cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-item-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-item-amount')[0]
+        var price = parseFloat(priceElement.innerText.replace('$',''))
+        var quantity = quantityElement.value
+        console.log(price * quantity)
+        total = total + (price * quantity)
+    }
+    document.querySelector('.cart-total').innerText = "$" + total
+}
